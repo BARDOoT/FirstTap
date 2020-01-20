b7a7f1d7b925b700a3bdfe6031f6a9351ab7f845
BARDOoT <92464862+BARDOoT@users.noreply.github.com>
2020-01-20 19:15:55 +0000

    minor text

--- project/js/script.js
+++ project/js/script.js
@@ -1,3 +1,9 @@
 
 
 function startsesh() {
+    var xmlhttp = new XMLHttpRequest();
+    xmlhttp.onreadystatechange = function() {
+        if (this.readyState == 4 && this.status == 200) {
+            document.getElementById("headings").innerHTML = this.responseText;
+        }
+    };
