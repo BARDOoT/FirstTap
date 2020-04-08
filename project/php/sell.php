21d4518182854a153c0d5892b7a26b5abbc76869
BARDOoT <92464862+BARDOoT@users.noreply.github.com>
2020-04-08 21:15:18 +0000

    Another one

--- project/php/sell.php
+++ project/php/sell.php
@@ -20,3 +20,7 @@
         // Check connection
         if ($conn->connect_error) {
             die("Connection failed: " . $conn->connect_error);
+        } else {
+    
+            $db_selected = mysqli_select_db($conn, 'kiye0230');
+            if (!$db_selected) {
