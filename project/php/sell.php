31ef669805e65678d6503d1e97f6267ab0dae040
BARDOoT <92464862+BARDOoT@users.noreply.github.com>
2020-05-14 16:00:49 +0000

    cmake stuff

--- project/php/sell.php
+++ project/php/sell.php
@@ -43,3 +43,5 @@
 					echo '<p>You cannot sell more than you have!</p><br><a href="../html/buysell.html">Click to return to the Buy/Sell page.</a>';
 					exit;
                 }
+
+                $total = $userbank - $amt;
