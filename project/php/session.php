b52599700f0b5ae78b52eda4dd1388f934905540
BARDOoT <92464862+BARDOoT@users.noreply.github.com>
2020-11-10 12:48:57 +0000

    Removed some commented code.

--- project/php/session.php
+++ project/php/session.php
@@ -1,4 +1,4 @@
 <?php 
 session_start();
       if($_SESSION["loggedin"] == 1){
-	echo '<a class="btn btn-primary" href="../html/home.html"><h3>Hom
+	echo '<a class="btn btn-primary" href="../html/home.html"><h3>Home
