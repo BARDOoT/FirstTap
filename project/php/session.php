276137564d33cd441cf91a4336d0269822ba9f57
BARDOoT <92464862+BARDOoT@users.noreply.github.com>
2020-11-30 14:29:00 +0000

    Build on Doug's server.

--- project/php/session.php
+++ project/php/session.php
@@ -2,4 +2,4 @@
 session_start();
       if($_SESSION["loggedin"] == 1){
 	echo '<a class="btn btn-primary" href="../html/home.html"><h3>Home</h3></a>';
-        echo '<a cla
+        echo '<a class=
