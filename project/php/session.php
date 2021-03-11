08a5f1293ed5ee028d133739b7339701a0208faa
BARDOoT <92464862+BARDOoT@users.noreply.github.com>
2021-03-11 15:52:20 +0000

    Kommentare

--- project/php/session.php
+++ project/php/session.php
@@ -2,4 +2,4 @@
 session_start();
       if($_SESSION["loggedin"] == 1){
 	echo '<a class="btn btn-primary" href="../html/home.html"><h3>Home</h3></a>';
-        echo '<a class="btn btn-primary" href="../html/buysell.html"><h3>Buy/Sell</h3
+        echo '<a class="btn btn-primary" href="../html/buysell.html"><h3>Buy/Sell</h3></a
