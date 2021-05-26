738ee38155195026095b4c893e20c946439981cc
BARDOoT <92464862+BARDOoT@users.noreply.github.com>
2021-05-26 20:08:03 +0000

    Readme.md edits

--- project/php/session.php
+++ project/php/session.php
@@ -4,4 +4,4 @@ session_start();
 	echo '<a class="btn btn-primary" href="../html/home.html"><h3>Home</h3></a>';
         echo '<a class="btn btn-primary" href="../html/buysell.html"><h3>Buy/Sell</h3></a>';
         echo '<a class="btn btn-primary" href="../html/send.html"><h3>Send</h3></a>';
-        echo
+        echo '
