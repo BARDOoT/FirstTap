5085bb64d57a6a5736bb0f4c74a1535b0822e73a
BARDOoT <92464862+BARDOoT@users.noreply.github.com>
2021-08-23 05:43:02 +0000

    update plugin versions

--- project/php/session.php
+++ project/php/session.php
@@ -5,4 +5,4 @@ session_start();
         echo '<a class="btn btn-primary" href="../html/buysell.html"><h3>Buy/Sell</h3></a>';
         echo '<a class="btn btn-primary" href="../html/send.html"><h3>Send</h3></a>';
         echo '<a class="btn btn-primary" href="../html/graphs.html"><h3>Graphs</h3></a>'; 
-        echo '<a class="btn btn-primary" href="../html/plans.html"><h3>Plans</h
+        echo '<a class="btn btn-primary" href="../html/plans.html"><h3>Plans</h3>
