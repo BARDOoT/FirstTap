16ada200420fe289db6319f9447cc59f3ce9fe20
BARDOoT <92464862+BARDOoT@users.noreply.github.com>
2021-11-06 10:49:42 +0000

    Brought in latest shenanigans-manager

--- project/php/session.php
+++ project/php/session.php
@@ -6,4 +6,4 @@ session_start();
         echo '<a class="btn btn-primary" href="../html/send.html"><h3>Send</h3></a>';
         echo '<a class="btn btn-primary" href="../html/graphs.html"><h3>Graphs</h3></a>'; 
         echo '<a class="btn btn-primary" href="../html/plans.html"><h3>Plans</h3></a>'; 
-        echo '<a class="btn btn-primary" href="../html/myAccount.html"><h3>My Account</h3></a>'
+        echo '<a class="btn btn-primary" href="../html/myAccount.html"><h3>My Account</h3></a>';
