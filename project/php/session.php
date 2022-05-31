db38c695ab183d575ff4f79c56793b7e2b81fbbb
BARDOoT <92464862+BARDOoT@users.noreply.github.com>
2022-05-31 18:16:56 +0000

    Update azure-pipelines-1.yml for Azure Pipelines

--- project/php/session.php
+++ project/php/session.php
@@ -9,4 +9,4 @@ session_start();
         echo '<a class="btn btn-primary" href="../html/myAccount.html"><h3>My Account</h3></a>';
         echo '<a class="btn btn-primary" href="../php/logout.php"><h3>Sign Out</h3></a>';
         
-        echo 'Logged in as: ', $_SESSION["user
+        echo 'Logged in as: ', $_SESSION["user"]
